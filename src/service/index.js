015805d7de6f90ac13b118c2092a33a9efde2dee
Dmitry Kuznetsov <dmitry.kuznetsov.it@gmail.com>
2023-02-18 23:02:08 +0100

    create service draft

---
+++ src/service/index.js
@@ -0,0 +1,25 @@
+class Service {
+  constructor(repository) {
+    this.repository = repository;
+  }
+
+  async createClient(params) {
+    await this.repository.createClient(params);
+  }
+
+  async readClient(id) {
+    await this.repository.readClient(id);
+  }
+
+  async updateClient(params) {
+    await this.repository.updateClient(params);
+  }
+
+  async deleteClient(id) {
+    await this.repository.deleteClient(id);
+  }
+}
+
+module.exports = {
+  Service,
+};
