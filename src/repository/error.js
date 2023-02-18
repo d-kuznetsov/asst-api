ecda3dfc19705e2c1594658549642e18e0341fb3
Dmitry Kuznetsov <dmitry.kuznetsov.it@gmail.com>
2023-02-18 17:20:53 +0100

    feat: create repository errors

---
+++ src/repository/error.js
@@ -0,0 +1,34 @@
+class RepositoryError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
+class RepositiryClientError extends RepositoryError {
+  constructor(message) {
+    super(message);
+  }
+}
+
+class RepositiryServerError extends RepositoryError {
+  constructor(message) {
+    super(message);
+  }
+}
+/* 
+function test() {
+  throw new RepositiryClientError("Some error");
+}
+
+function run() {
+  test();
+}
+
+run(); */
+
+module.exports = {
+  RepositoryError,
+  RepositiryClientError,
+  RepositiryServerError,
+};
