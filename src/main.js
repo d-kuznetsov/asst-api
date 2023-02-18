f97a953f8f96254e5de6e82c5da417ff9ced37ba
Dmitry Kuznetsov <dmitry.kuznetsov.it@gmail.com>
2023-02-18 15:36:24 +0100

    try to test createClient method

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ const Repositiry = require("./repository").MongoDB;
 const run = async () => {
   const repository = new Repositiry();
   await repository.connect();
+  const client = { name: "A GmbH", email: "a.gmbh@example.com" };
+  await repository.createClient(client);
+  await repository.disconnect();
 };
 
 run();
