ed0117d2e5439b621520e80e82da8edc3fcc2995
Dmitry Kuznetsov <dmitry.kuznetsov.it@gmail.com>
2023-02-18 21:27:24 +0100

    feat: cover repository with tests

---
+++ src/tests/repository.test.js
@@ -0,0 +1,48 @@
+const Repositiry = require("../repository").MongoDB;
+
+describe("client", () => {
+  const repository = new Repositiry();
+  const mockClient = {
+    name: "John Smith",
+    email: "smith@gmail.com",
+  };
+  let clientId;
+
+  beforeAll(async () => {
+    await repository.connect();
+  });
+
+  test("create client", async () => {
+    clientId = await repository.createClient(mockClient);
+    expect(clientId).toBeTruthy();
+  });
+
+  test("read client", async () => {
+    const createdClient = await repository.readClient(clientId);
+    expect(createdClient).toEqual(mockClient);
+  });
+
+  test("update client", async () => {
+    const newEmail = "john.smith@gmail.com";
+    await repository.updateClient({
+      id: clientId,
+      email: newEmail,
+    });
+    const client = await repository.readClient(clientId);
+    expect(client.email).toEqual(newEmail);
+  });
+
+  test("delete client", async () => {
+    expect.assertions(1);
+    try {
+      await repository.deleteClient(clientId);
+      await repository.readClient(clientId);
+    } catch (err) {
+      expect(err.message).toMatch("No record found with given id");
+    }
+  });
+
+  afterAll(async () => {
+    await repository.disconnect();
+  });
+});
