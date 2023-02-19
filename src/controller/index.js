113900bfa816ebfa2e50f58eea8494f48851fac3
Dmitry Kuznetsov <dmitry.kuznetsov.it@gmail.com>
2023-02-19 23:41:55 +0100

    send code depends on suctom error

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,3 +1,7 @@
+const { STATUS_CODES } = require("http");
+
+const { ClientError } = require("../error");
+
 class Controller {
   constructor(service) {
     this.service = service;
@@ -7,8 +11,12 @@ class Controller {
       const client = await this.service.readClient(req.params.id);
       reply.send(client);
     } catch (err) {
+      if (err instanceof ClientError) {
+        reply.code(400).send(STATUS_CODES[400]);
+        return;
+      }
       console.error(err);
-      reply.code(500).send(err);
+      reply.code(500).send(STATUS_CODES[500]);
     }
   }
 }
