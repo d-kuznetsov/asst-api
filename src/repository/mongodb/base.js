7860c16beef8465e910030b6a8e4d378a680084e
Dmitry Kuznetsov <dmitry.kuznetsov.it@gmail.com>
2023-02-25 20:21:18 +0100

    ref: split mongodb module into two separate

--- src/repository/mongodb/base.js
+++ src/repository/mongodb/base.js
@@ -1,5 +1,5 @@
 const { MongoClient, ObjectId } = require("mongodb");
-const { ServerError, ClientError, ERR_MESSAGES } = require("../error");
+const { ServerError, ClientError, ERR_MESSAGES } = require("../../error");
 
 const checkId = (id) => {
   if (!ObjectId.isValid(id)) {
@@ -141,53 +141,7 @@ class MongoDbBase {
   }
 }
 
-class MongoDB extends MongoDbBase {
-  constructor(uri) {
-    super(uri);
-  }
-
-  async createUser(params) {
-    return this._insertOne("users", params);
-  }
-
-  async findUser(params) {
-    return this._findOne("users", params);
-  }
-
-  async createClient(params) {
-    return this._insertOne("clients", params);
-  }
-
-  async findClientById(id) {
-    checkId(id);
-    return this._findOne("clients", { id });
-  }
-
-  async updateClient(params) {
-    checkId(params.id);
-    return this._updateOne("clients", params);
-  }
-
-  async deleteClientById(id) {
-    checkId(id);
-    this._deleteOne("clients", { id });
-  }
-
-  async findClients(params = {}) {
-    return this._find("clients", params);
-  }
-
-  async createAssistant(params) {
-    return this._insertOne("assistants", params);
-  }
-
-  async findAssistant(query) {
-    return this._findOne("assistants", query);
-  }
-
-  async findAssistants(params = {}) {
-    return this._find("assistants", params);
-  }
-}
-
-module.exports = MongoDB;
+module.exports = {
+  MongoDbBase,
+  checkId,
+};
