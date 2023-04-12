50dd000c7f153a0c2297a2ce21859fa4fd428856
Dmitry Kuznetsov <dmitry.kuznetsov.it@gmail.com>
2023-04-12 10:47:54 +0200

    remove @fastify/multipart

--- src/server.js
+++ src/server.js
@@ -1,13 +1,9 @@
 const Fastify = require("fastify");
-const fstMultipart = require("@fastify/multipart");
 const auth = require("./auth");
 const swagger = require("./swagger");
 
 const createServer = (router, logger) => {
   const fastify = Fastify({ logger });
-  fastify.register(fstMultipart, {
-    attachFieldsToBody: "keyValues",
-  });
   fastify.register(auth);
   fastify.register(swagger);
   fastify.register(router);
